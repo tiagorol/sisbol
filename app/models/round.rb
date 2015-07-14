be1dec90965b5584e80da355ea7d75e530a6e877
tiago <tiago.rol@gmail.com>
2015-07-14 20:01:20 -0300

    Validação

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,10 +1,5 @@
 class Round < ActiveRecord::Base
   belongs_to :championship
   validates :championship, presence: true
-  validates :name, length: {
-      minimum: 5,
-      maximum: 20,
-      too_short: "must have at least %{count} words",
-      too_long: "must have at most %{count} words"
-  }
+  validates :name, length: {minimum: 5, maximum: 20}
 end
