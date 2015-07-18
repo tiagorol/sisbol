1c118367e01e1d5070ac8bec47dbdc1f1ea4f2cf
tiago <tiago.rol@gmail.com>
2015-07-18 16:36:24 -0300

    Implementação testes automatizados. Ainda em desenvolvimento...

--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RoundTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Validation Fields Required" do
+    round = Round.new
+    assert_not(round.save, "Nao validou corretamente")
+  end
+
+  test "Save" do
+    round = Round.create(name: "Rodada 18", championship: Championship.first)
+    assert(round.save, "Nao salvou corretamente")
+    assert_equal(Round.last.name, "Rodada 18", "Nao salvou corretamente");
+  end
+
+  test "Find All" do
+    rounds = Round.all
+    assert_equal(rounds.size, 2, "Nao buscou corretamente");
+  end
+
 end
