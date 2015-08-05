51c6fc6d99c8dcadb1df37b893dcec2feeb305ea
tiago <tiago.rol@gmail.com>
2015-08-04 21:15:15 -0300

    Testes

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,4 +6,15 @@ class SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should create session" do
+    post :create, session: {email: "joao.silva@gmail.com", password: "xyz123456"}
+    assert_redirected_to root_path
+  end
+
+  test "should destroy session" do
+    delete :destroy
+    assert_nil  session[:user_id]
+    assert_redirected_to login_path
+  end
+
 end
