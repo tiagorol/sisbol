e134e610c1b6db6cd7587783b5d274d601474993
tiago <tiago.rol@gmail.com>
2015-09-13 16:37:37 -0300

    Mensagem validação altera senha

--- app/controllers/change_password_controller.rb
+++ app/controllers/change_password_controller.rb
@@ -1,42 +1,45 @@
 class ChangePasswordController < ApplicationController
-  before_action :validade_current_password, only: [:change]
-  before_action :validade_password, only: [:change]
-  before_action :validade_confirmation_password, only: [:change]
+  before_action :validate_current_password, only: [:change]
+  before_action :validate_password, only: [:change]
+  before_action :validate_confirmation_password, only: [:change]
+
+  def new
+  end
 
   def change
     new_password = params[:change_password][:new_password]
     @user.update(password: new_password)
-    #MENSAGEM DE SENHAS ALTERADA COM SUCESSO
+    flash.now[:notice] = t(:message_updated)
     render 'new'
   end
 
   private
 
-    def validade_password
+    def validate_password
       new_password = params[:change_password][:new_password]
 
       if new_password.length < 5 || new_password.length > 100
-        #MENSAGEM DE SENHAS COM TAMANHO INVALIDO
+        flash.now[:error] = t(:message_validate_length_password)
         render 'new'
       end
     end
 
-    def validade_confirmation_password
+    def validate_confirmation_password
       new_password = params[:change_password][:new_password]
       confirmation_password = params[:change_password][:confirmation_password]
 
       if !new_password.eql?confirmation_password
-        #MENSAGEM DE SENHAS E CONFIRMAÇÃO DE SENHA DIFERENTE
+        flash.now[:error] = t(:message_validate_confirmation_password)
         render 'new'
       end
     end
 
-    def validade_current_password
+    def validate_current_password
       current_password = params[:change_password][:current_password]
       @user = User.find(session[:user_id])
 
       if !@user.authenticate(current_password)
-        #MENSAGEM DE SENHA ATUAL INVALDA
+        flash.now[:error] = t(:message_validate_current_password)
         render 'new'
       end
     end
