9bd0cde2951f41523d2e37e8c9255395bea53940
tiago <tiago.rol@gmail.com>
2015-09-15 20:04:53 -0300

    Correção dos testes welcome

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  setup do
+    session[:user_id] = users(:one).id
+  end
+
   test "should get index" do
     get :index
     assert_response :success
