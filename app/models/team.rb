788e1038c7d6e65d77eca8278559ab2126588b68
tiago <tiago.rol@gmail.com>
2015-10-06 20:47:51 -0300

    Ajuste cadastro de time

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,11 @@ class Team < ActiveRecord::Base
   validates :full_description, length: {minimum: 5, maximum: 255}, presence: true
   has_attached_file :crest, styles: { medium: '300x300>', thumb: '100x100>'}
   validates_attachment_content_type :crest, content_type: /\Aimage\/.*\Z/
+
+  def url_crest_thumb
+    if self.crest_file_name.nil?
+      return "crest_thumb.png"
+    end
+    return self.crest.url(:thumb)
+  end
 end
