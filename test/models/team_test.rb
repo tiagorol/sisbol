5fa9f6dc74e987f145a3c6ccf6c49d352cc8121e
Gustavo <guspb94@gmail.com>
2015-11-14 13:34:14 -0300

    Testes de Time

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Validation Fields Required" do
+    team = Team.new
+    assert_not(team.save, "Nao validou corretamente")
+  end
+
+  test "Save" do
+    team = Team.create(description: "Ferroviario", full_description: "Ferroviario Atletico Clube")
+    assert(team.save, "Nao salvou corretamente")
+    assert_equal(Team.last.description, "Ferroviario", "Nao salvou corretamente")
+  end
+
+  test "Update" do
+    team = Team.first
+    assert(team.update(description: "Ferrao"), "Nao atualizou corretamente")
+    assert_equal(Team.first.description, "Ferrao", "Nao atualizou corretamente")
+  end
+
+  test "Destroy" do
+    team = Team.find_by(description: "Icasa")
+    team.destroy
+    assert_equal(Team.all.size, 2, "Nao excluiu corretamente");
+  end
+
+  test "Find All" do
+    assert_equal(Team.all.size, 3, "Nao buscou corretamente")
+  end
 end
